996d36a2e5b24af5d9e08bee9f9ae59038f8b679
Dirk Lüth <info@qoopido.com>
2015-10-22 13:10:11 +0200

    [UPDATE] add favicon

--- site/index.php
+++ site/index.php
@@ -11,6 +11,10 @@
 	<meta name="author" content="Dirk Lüth <info@qoopido.com>" />
 	<meta name="theme-color" content="#5d407f">
 
+	<link rel="shortcut icon" href="./assets/images/favicon.png" />
+	<link rel="icon" href="./assets/images/favicon.png" />
+	<link rel="apple-touch-icon" href="./assets/images/favicon.png" type="image/png" />
+
 	<meta itemprop="url" content="http://nucleus.qoopido.com" />
 	<meta itemprop="name" content="Qoopido.nucleus - modular JavaScript utility library" />
 	<meta itemprop="description" content="Lightweight, atomic and modular JavaScript utility library that strongly encourages the creation and usage of small yet flexible, reusable and easily maintainable modules." />
@@ -35,14 +39,14 @@
 <body>
 
 <main role="main" itemprop="mainContentOfPage">
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="intro">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-intro" style="background-color: rgb(102, 100, 102);">
 		<meta itemprop="image" content="./assets/images/1.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#intro" />
 		<h1><span itemprop="headline">What can Qoopido.<wbr />nucleus do for you?</span></h1>
 		<div><p itemprop="description">Nucleus is a lightweight, atomic and modular JavaScript utility library. It strongly encourages the creation and usage of small yet flexible, reusable and easily maintainable modules. Nucleus utilizes Qoopido.demand as its caching loader.</p></div>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="base">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-base" style="background-color: rgb(126, 150, 34);">
 		<meta itemprop="image" content="./assets/images/2.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#base" />
 		<h2><span itemprop="headline">simple & lightweight inheritance</span></h2>
@@ -50,7 +54,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="emitter">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-emitter" style="background-color: rgb(157, 115, 140);">
 		<meta itemprop="image" content="./assets/images/3.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#emitter" />
 		<h2><span itemprop="headline">events made easy & fun</span></h2>
@@ -58,7 +62,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="support">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-support" style="background-color: rgb(175, 114, 88);">
 		<meta itemprop="image" content="./assets/images/4.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#support" />
 		<h2><span itemprop="headline">extendible feature detection</span></h2>
@@ -66,7 +70,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="dom">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-dom" style="background-color: rgb(111, 161, 55);">
 		<meta itemprop="image" content="./assets/images/5.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#dom" />
 		<h2><span itemprop="headline">intuitive DOM manipulation</span></h2>
@@ -74,7 +78,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="domElementAppear">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-domElementAppear" style="background-color: rgb(209, 207, 213);">
 		<meta itemprop="image" content="./assets/images/6.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#domElementAppear" />
 		<h2><span itemprop="headline">viewport-aware DOM elements</span></h2>
@@ -82,7 +86,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="componentSense">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-componentSense" style="background-color: rgb(195, 159, 120);">
 		<meta itemprop="image" content="./assets/images/7.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#componentSense" />
 		<h2><span itemprop="headline">CSS media queries in JavaScript</span></h2>
@@ -90,7 +94,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="transportXhr">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-transportXhr" style="background-color: rgb(111, 68, 69);">
 		<meta itemprop="image" content="./assets/images/8.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#transportXhr" />
 		<h2><span itemprop="headline">Ajax for human beings</span></h2>
@@ -98,7 +102,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="task">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-task" style="background-color: rgb(146, 66, 35);">
 		<meta itemprop="image" content="./assets/images/9.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#task" />
 		<h2><span itemprop="headline">WebWorker done right</span></h2>
@@ -106,7 +110,7 @@
 		<a href="#snippet" title="show snippet">{ }</a>
 	</section>
 
-	<section itemscope itemtype="http://schema.org/WebPageElement" id="componentIterator">
+	<section itemscope itemtype="http://schema.org/WebPageElement" id="section-componentIterator" style="background-color: rgb(60, 128, 184);">
 		<meta itemprop="image" content="./assets/images/10.jpg" />
 		<meta itemprop="url" content="http://nucleus.qoopido.com/#componentIterator" />
 		<h2><span itemprop="headline">flexible iterators</span></h2>
