d052b3f467bf04b13cf30f5aa25e7b6a5648754f
Dirk Lüth <info@qoopido.com>
2016-01-29 08:46:43 +0100

    [UPDATE] build

--- dist/component/sense.js
+++ dist/component/sense.js
@@ -1,3 +1,3 @@
-/*! /component/sense 1.0.7 | http://nucleus.qoopido.com | (c) 2016 Dirk Lueth */
-!function(t){"use strict";function e(t,e){function i(t){var i=this["super"].call(this),c=e(t);return n[i.uuid]=c,c.addListener(function(){i.emit(c.matches===!0?"match":"unmatch")}),i}var n={};return i.prototype={get matches(){var t=n[this.uuid];return t?t.matches:void 0}},t.extend(i)}provide(["../emitter",t.matchMedia||"../polyfill/window/matchmedia"],e)}(this);
+/*! /component/sense 1.0.8 | http://nucleus.qoopido.com | (c) 2016 Dirk Lueth */
+!function(t){"use strict";function e(t,e){function i(i){var c=t.prototype.constructor.call(this),r=e(i);return n[c.uuid]=r,r.addListener(function(){c.emit(r.matches===!0?"match":"unmatch")}),c}var n={};return i.prototype={get matches(){var t=n[this.uuid];return t?t.matches:void 0}},t.extend(i)}provide(["../emitter",t.matchMedia||"../polyfill/window/matchmedia"],e)}(this);
 //# sourceMappingURL=sense.js.map
