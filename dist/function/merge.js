d190d317110d2e7c9d77c392f30b7447b04029ac
Dirk Lüth <dlueth@Dirks-MacBook-Pro.local>
2017-01-18 10:30:57 +0100

    build

--- dist/function/merge.js
+++ dist/function/merge.js
@@ -1,3 +1,3 @@
-/*! /function/merge 2.0.2 | http://nucleus.qoopido.com | (c) 2016 Dirk Lueth */
-!function(n){"use strict";function t(t){return function r(){for(var e,i,u,o,a,c=arguments[0],f=1;(e=arguments[f])!==n;f++)for(i in e)u=c[i],a=e[i],a!==n&&(t(a)?(o=t(u),u=a.length!==n?o&&u.length!==n?u:[]:o&&u.length===n?u:{},c[i]=r(u,a)):c[i]=a);return c}}provide(["/demand/validator/isObject"],t)}();
+/*! /function/merge 2.0.3 | http://nucleus.qoopido.com | (c) 2017 Dirk Lueth */
+!function(n){"use strict";function t(t){return function r(){for(var e,i,o,u,c,f=arguments[0],a=1;(e=arguments[a])!==n;a++)for(i in e)o=f[i],c=e[i],c!==n&&(t(c)?(u=t(o),o=c.length!==n?u&&o.length!==n?o:[]:u&&o.length===n?o:{},f[i]=r(o,c)):f[i]=c);return f}}provide(["/demand/validator/isObject"],t)}();
 //# sourceMappingURL=merge.js.map
