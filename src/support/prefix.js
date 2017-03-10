6d8aa7ce258fdc04128783204e99aaf665ae2dca
Dirk Lüth <info@qoopido.com>
2017-03-10 17:38:59 +0100

    fix bug where FF could not detect any vendor-prefix

--- src/support/prefix.js
+++ src/support/prefix.js
@@ -1,45 +1,44 @@
 /**
- * @use /demand/function/iterate
- *
  * @require ../function/string/ucfirst
  */
 
-(function(document) {
+(function(global, document) {
 	'use strict';
 
-	function definition(iterate, functionStringUcfirst) {
-		var styles           = document.createElement('div').style,
-			regexMatchPrefix = /^(Moz|WebKit|Khtml|ms|O|Icab)(?=[A-Z])/,
-			prefix           = null;
-
+	function definition(functionStringUcfirst) {
+		var styles           = global.getComputedStyle(document.documentElement, ''),
+			regexMatchPrefix = /^-(webkit|apple|moz|ms|o)-/,
+			prefix           = null,
+			i = 0, property;
+		
 		return function supportPrefix() {
 			if(prefix === null) {
 				prefix = false;
-
-				iterate(styles, function(property) {
+				
+				for(; (property = styles[i]); i++) {
 					if(regexMatchPrefix.test(property)) {
-						prefix = property.match(regexMatchPrefix)[0];
-
-						return false;
+						prefix = property.match(regexMatchPrefix)[1];
+						
+						break;
 					}
-				});
-
+				}
+				
 				if(!prefix && 'WebkitOpacity' in styles) {
 					prefix = 'WebKit';
 				}
-
+				
 				if(!prefix && 'KhtmlOpacity' in styles) {
 					prefix = 'Khtml';
 				}
-
+				
 				if(prefix) {
 					prefix = [prefix.toLowerCase(), functionStringUcfirst(prefix.toLowerCase()), prefix];
 				}
 			}
-
-			return prefix;
+			
+			return prefix || [];
 		};
 	}
 
-	provide([ '/demand/function/iterate', '../function/string/ucfirst' ], definition);
-}(document));
+	provide([ '../function/string/ucfirst' ], definition);
+}(this, document));
