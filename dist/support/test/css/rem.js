d99c1f2c16fdb6c4af3f8ede3d92e2340128410b
Dirk Lüth <dlueth@Dirks-MBP.fritz.box>
2018-02-10 00:33:57 +0100

    bump & build

--- dist/support/test/css/rem.js
+++ dist/support/test/css/rem.js
@@ -1,2 +1,2 @@
-!function(e){"use strict";provide(["/demand/pledge"],function(t){var r=t.defer(),n=e.createElement("div").style;try{n.fontSize="3rem"}catch(e){}return/rem/.test(n.fontSize)?r.resolve():r.reject(),r.pledge})}(document);
+!function(e){"use strict";provide(["/demand/pledge"],function(t){var r=t.defer(),n=e.createElement("div").style,c="fontSize";try{n[c]="3rem"}catch(e){}return/rem/.test(n[c])?r.resolve():r.reject(),r.pledge})}(document);
 //# sourceMappingURL=rem.js.map
